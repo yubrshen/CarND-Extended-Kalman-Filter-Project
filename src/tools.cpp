d46a5827a37385ddc3bebba126c929116162609a
Yu Shen <yubrshen@gmail.com>
2017-03-28 21:20:29 +0800

    implement RMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,10 +11,25 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+
+  if ((0 < estimations.size()) && (estimations.size() == ground_truth.size())) {
+    VectorXd r, s;
+    for(unsigned int i=0; i < estimations.size(); ++i){
+      r = (estimations[ i ] - ground_truth[ i ]);
+      s = r.array()*r.array();
+      rmse += s;
+    }
+    rmse = rmse/estimations.size();
+    rmse = rmse.array().sqrt();
+  } else {
+    cout << "Invalid estimation or ground_truth data" << endl;
+  }
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
