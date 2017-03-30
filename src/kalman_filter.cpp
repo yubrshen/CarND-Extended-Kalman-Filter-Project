da198f5d323dc9558f6c9d7780098b03bdf18010
Yu Shen <yubrshen@gmail.com>
2017-03-30 17:55:20 +0800

    EKFUpdate, tried nonlinear transformation.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -49,7 +49,39 @@ void KalmanFilter::Update(const VectorXd &z) {
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
-  TODO:
+  DONE:
     * update the state by using Extended Kalman Filter equations
   */
+  // VectorXd z_pred = H_ * x_;
+
+  // The following code compute y by the accurate but nonlinear equation:
+  VectorXd z_pred(2);
+
+  z_pred <<
+    sqrt(x_[0]*x_[0] + x_[1]*x_[1]), atan2(x_[1], x_[0]);
+
+  VectorXd y = z - z_pred;
+
+  // Begin adjust y[1]
+  // make sure y[1], the angle is within [-pi, pi]
+  float full_circle = 2*M_PI;
+  while (y[1] < -M_PI) {
+    y[1] += full_circle;
+  }
+  while (M_PI < y[1]) {
+    y[1] -= full_circle;
+  }
+  // End adjust y[1]
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
