52e5cf1a478e5b80505a879af4755204f02e3afb
Yu Shen <yubrshen@gmail.com>
2017-03-31 07:40:20 +0800

    add checking GoodMeasurement

--- src/main.cpp
+++ src/main.cpp
@@ -136,35 +136,37 @@ int main(int argc, char* argv[]) {
   for (size_t k = 0; k < N; ++k) {
     // start filtering from the second frame (the speed is unknown in the first
     // frame)
-    fusionEKF.ProcessMeasurement(measurement_pack_list[k]);
+    if (fusionEKF.GoodMeasurement(measurement_pack_list[k])) {
+      fusionEKF.ProcessMeasurement(measurement_pack_list[k]);
 
-    // output the estimation
-    out_file_ << fusionEKF.ekf_.x_(0) << "\t";
-    out_file_ << fusionEKF.ekf_.x_(1) << "\t";
-    out_file_ << fusionEKF.ekf_.x_(2) << "\t";
-    out_file_ << fusionEKF.ekf_.x_(3) << "\t";
-
-    // output the measurements
-    if (measurement_pack_list[k].sensor_type_ == MeasurementPackage::LASER) {
       // output the estimation
-      out_file_ << measurement_pack_list[k].raw_measurements_(0) << "\t";
-      out_file_ << measurement_pack_list[k].raw_measurements_(1) << "\t";
-    } else if (measurement_pack_list[k].sensor_type_ == MeasurementPackage::RADAR) {
-      // output the estimation in the cartesian coordinates
-      float ro = measurement_pack_list[k].raw_measurements_(0);
-      float phi = measurement_pack_list[k].raw_measurements_(1);
-      out_file_ << ro * cos(phi) << "\t"; // p1_meas
-      out_file_ << ro * sin(phi) << "\t"; // ps_meas
+      out_file_ << fusionEKF.ekf_.x_(0) << "\t";
+      out_file_ << fusionEKF.ekf_.x_(1) << "\t";
+      out_file_ << fusionEKF.ekf_.x_(2) << "\t";
+      out_file_ << fusionEKF.ekf_.x_(3) << "\t";
+
+      // output the measurements
+      if (measurement_pack_list[k].sensor_type_ == MeasurementPackage::LASER) {
+        // output the estimation
+        out_file_ << measurement_pack_list[k].raw_measurements_(0) << "\t";
+        out_file_ << measurement_pack_list[k].raw_measurements_(1) << "\t";
+      } else if (measurement_pack_list[k].sensor_type_ == MeasurementPackage::RADAR) {
+        // output the estimation in the cartesian coordinates
+        float ro = measurement_pack_list[k].raw_measurements_(0);
+        float phi = measurement_pack_list[k].raw_measurements_(1);
+        out_file_ << ro * cos(phi) << "\t"; // p1_meas
+        out_file_ << ro * sin(phi) << "\t"; // ps_meas
+      }
+
+      // output the ground truth packages
+      out_file_ << gt_pack_list[k].gt_values_(0) << "\t";
+      out_file_ << gt_pack_list[k].gt_values_(1) << "\t";
+      out_file_ << gt_pack_list[k].gt_values_(2) << "\t";
+      out_file_ << gt_pack_list[k].gt_values_(3) << "\n";
+
+      estimations.push_back(fusionEKF.ekf_.x_);
+      ground_truth.push_back(gt_pack_list[k].gt_values_);
     }
-
-    // output the ground truth packages
-    out_file_ << gt_pack_list[k].gt_values_(0) << "\t";
-    out_file_ << gt_pack_list[k].gt_values_(1) << "\t";
-    out_file_ << gt_pack_list[k].gt_values_(2) << "\t";
-    out_file_ << gt_pack_list[k].gt_values_(3) << "\n";
-
-    estimations.push_back(fusionEKF.ekf_.x_);
-    ground_truth.push_back(gt_pack_list[k].gt_values_);
   }
 
   // compute the accuracy (RMSE)
