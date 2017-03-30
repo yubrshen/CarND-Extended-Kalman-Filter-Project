91599bbcc371213e5a16d9269a8a754c3ceda942
Yu Shen <yubrshen@gmail.com>
2017-03-30 14:15:53 +0800

    add noise_ax and noise_ay

--- src/FusionEKF.h
+++ src/FusionEKF.h
@@ -38,6 +38,10 @@ private:
   // previous timestamp
   long previous_timestamp_;
 
+  // for Q matrix:
+  float noise_ax = 9;
+  float noise_ay = 9;
+
   // tool object used to compute Jacobian and RMSE
   Tools tools;
   Eigen::MatrixXd R_laser_;
